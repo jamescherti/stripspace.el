32855b8d20b89d16fc95617bce498664602927ec
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-03-20 16:00:27 -0400

    Release: 1.0.0

--- stripspace.el
+++ stripspace.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 0.9.9
+;; Version: 1.0.0
 ;; URL: https://github.com/jamescherti/stripspace.el
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "24.3"))
